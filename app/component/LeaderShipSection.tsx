e4508670a656e27c7cd8904e13bbbf104e61ed92
Prasadmali1311 <prasadmalithird@gmail.com>
2025-11-14 14:04:06 +0530

    -Debugger added for image loader

--- app/component/LeaderShipSection.tsx
+++ app/component/LeaderShipSection.tsx
@@ -10,7 +10,7 @@ const LeadershipSection = () => {
     {
       name: "Mr. Rahul Kangane",
       title: "Chairman and Managing Director",
-      description: "13 years of expertise in broking and wealth management, with a focus on portfolio strategy. His sharp market insights fuel the company's growth and success.",
+      description: "15 years of expertise in broking and wealth management, with a focus on portfolio strategy. His sharp market insights fuel the company's growth and success.",
       image: "rahulsir.jpeg"
     },
     {
