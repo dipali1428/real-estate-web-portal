aff89553dc63433b1f8e0562039bc8528839ac0d
infinity-software-dev <iasoftware2025@gmail.com>
2025-12-04 11:13:13 +0530

    updated api.ts

--- app/services/api.ts
+++ app/services/api.ts
@@ -2,19 +2,20 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://192.168.1.83:5000", // ✅ backend base URL
+  baseURL: "https://api.infinityarthvishva.com", // ✅ backend base URL
   // "http://192.168.1.83:5000"
   // "https://api.infinityarthvishva.com"
+  withCredentials: true, // ⭐ SEND COOKIES AUTOMATICALLY
   headers: { "Content-Type": "application/json" },
 });
 
-api.interceptors.request.use(
-  (config) => {
-    const token = typeof window !== "undefined" ? document.cookie.match(/authToken=([^;]+)/)?.[1] : null;
-    if (token) config.headers.Authorization = `Bearer ${token}`;
-    return config;
-  },
-  (error) => Promise.reject(error)
-);
+// api.interceptors.request.use(
+//   (config) => {
+//     const token = typeof window !== "undefined" ? document.cookie.match(/authToken=([^;]+)/)?.[1] : null;
+//     if (token) config.headers.Authorization = `Bearer ${token}`;
+//     return config;
+//   },
+//   (error) => Promise.reject(error)
+// );
 
 export default api;
