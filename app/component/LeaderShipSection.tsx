5df693866ffca9c3edb3ffc44a2de3dbccffcaea
smrutirokade <smruti9802@gmail.com>
2025-12-11 17:37:02 +0530

    added new image in leadership section

--- app/component/LeaderShipSection.tsx
+++ app/component/LeaderShipSection.tsx
@@ -11,7 +11,7 @@ const LeadershipSection = () => {
       name: "Mr. Rahul Kangane",
       title: "Chairman and Managing Director",
       description: "15 years of expertise in broking and wealth management, with a focus on portfolio strategy. His sharp market insights fuel the company's growth and success.",
-      image: "/leader/rahulsir.jpeg"
+      image: "/leader/rahulsir4.jpeg"
     },
     {
       name: "Mr. Pravin Marathe",
