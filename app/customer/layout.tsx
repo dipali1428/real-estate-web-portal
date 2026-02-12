23bf95c6ee7d526c0a06bdfe00e65ced7ace963b
infinity-software-dev <iasoftware2025@gmail.com>
2026-02-12 17:05:37 +0530

    Coming soon page added in customer portal

---
+++ app/customer/layout.tsx
@@ -0,0 +1,29 @@
+import DashboardSidebar from "../component/Sidebar";
+import DashboardHeader from "../dashboard/components/DashboardHeader";
+import { Toaster } from "react-hot-toast";
+// import { cookies } from "next/headers";
+// import jwt from "jsonwebtoken";
+
+export default async function DashboardLayout({ children }: { children: React.ReactNode }) {
+    // const token = (await cookies()).get("authToken")?.value;
+    // const decoded: any = token ? jwt.decode(token) : null;
+    const role = "CUSTOMER";
+
+    return (
+        <div className="flex h-screen bg-gray-50 font-sans">
+
+            <Toaster position="top-right" />
+
+            <div className="hidden md:block">
+                <DashboardSidebar role={role} />
+            </div>
+
+            <div className="flex-1 flex flex-col min-h-screen w-full overflow-x-hidden">
+                <DashboardHeader role={role} />
+                <main className="flex-1 overflow-y-auto max-h-screen">
+                    {children}
+                </main>
+            </div>
+        </div>
+    );
+}
