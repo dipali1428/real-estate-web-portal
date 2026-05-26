15bb86d4ab87d4ed6d7476b1e7eb980287a2c267
infinity-software-dev <iasoftware2025@gmail.com>
2026-05-26 10:33:25 +0530

    - fix: switch image hostname from S3 to CloudFront

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,13 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: "https",
+        hostname: "diva2ast20wqu.cloudfront.net",
+        pathname: "/**",
+      },
+      // Keep the old S3 one temporarily until you confirm
+      // everything works, then remove it
+      {
+        protocol: "https",
         hostname: "infinity-client-documents.s3.ap-south-1.amazonaws.com",
         pathname: "/**",
       },
